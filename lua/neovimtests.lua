652574a9f59f02cdd50acff2ef3b56b8036e188b
TheMusicBoy <TheMusicBoy666@mail.ru>
2024-08-11 19:47:25 +0300

    Test plugin

---
+++ lua/neovimtests.lua
@@ -0,0 +1,11 @@
+local api = vim.api
+
+local function command(name, callback, opts) api.nvim_create_user_command(name, callback, opts) end
+
+local function setup()
+    command("Greet", function() print("Hello, world!") end, {nargs = 0})
+end
+
+return {
+    setup = setup
+}
